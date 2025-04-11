fe994cebd1ab3c31d015e3658e2eba92cf416baf
seemmaa <semahodali@gmail.com>
2025-04-11 16:26:59 +0200

    add custom models for add & update

---
+++ EmployeeManagement/Models/CustomDepartment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class CustomDepartment
+    {
+        [Required]
+        [DefaultValue("")]
+        public string Name { get; set; }
+        [DefaultValue("")]
+        public string DepartmentSupervisor { get; set; }
+    }
+}
---
+++ EmployeeManagement/Models/CustomEmployee.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace EmployeeManagement.Models
+{
+    public class CustomEmployee
+    {
+        [DefaultValue("")]
+        public string FirstName { get; set; }
+        [DefaultValue("")]
+        public string LastName { get; set; }
+        [DefaultValue("1990-01-01T00:00:00")]
+        public DateTime DateOfBirth { get; set; }
+        [DefaultValue("1990-01-01T00:00:00")]
+        public DateTime DateOfEmployment { get; set; }
+        [DefaultValue("1990-01-01T00:00:00")]
+        public DateTime EndOfServiceDate { get; set; }
+        [Required]
+        public int YearsOfService
+        {
+            get
+            {
+                DateTime endDate = EndOfServiceDate == DateTime.MinValue ? DateTime.Now : EndOfServiceDate;
+                int years = endDate.Year - DateOfEmployment.Year;
+                return years;
+            }
+        }
+        [DefaultValue("")]
+        public string Position { get; set; }
+
+        public int? DepartmentId { get; set; }
+
+        public bool? IsActive { get; set; }
+
+    }
+}
+
